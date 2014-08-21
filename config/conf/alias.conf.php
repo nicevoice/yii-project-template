f1cca8f8538fc6200857a2c4be300cf228b2fe68
Rogee <rogeecn@gmail.com>
2014-08-21 23:32:47 +0800

    完成内容渲染

--- config/conf/alias.conf.php
+++ config/conf/alias.conf.php
@@ -1,12 +1,13 @@
 <?php
 $alias = array(
-    'root'   => PATH_BASE,
-    'common' => PATH_BASE . '/common',
+    'root'        => PATH_BASE,
+    'common'      => PATH_BASE . '/common',
     'application' => PATH_BASE . '/common',
-    'com'    => PATH_BASE . '/common/components',
-    'vendor' => PATH_BASE . '/common/vendor',
-    'hook'   => PATH_BASE . '/common/hook',
-    'upload' => PATH_BASE . '/upload'
+    'ext'         => PATH_BASE . '/common/extensions',
+    'com'         => PATH_BASE . '/common/components',
+    'vendor'      => PATH_BASE . '/common/vendor',
+    'hook'        => PATH_BASE . '/common/hook',
+    'upload'      => PATH_BASE . '/upload'
 );
 
 foreach ($alias as $key => $value) {
