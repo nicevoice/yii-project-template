3c48aedaafcdb1266b48fcb7ba10eaa6d47e6223
Rogee <rogeecn@gmail.com>
2014-08-22 00:08:57 +0800

    完成内容渲染

--- common/controllers/IndexController.php
+++ common/controllers/IndexController.php
@@ -1,27 +1,29 @@
 <?php
+
 class IndexController extends BaseController
 {
-    public function actionIndex ()
+    public function actionIndex()
     {
-        $dataProvider=new CActiveDataProvider('Article', array(
-            'criteria'=>array(
-                'order'=>'id DESC',
-            ),
-            'pagination'=>array(
-                'pageSize'=>20,
-                'route' => 'index/index',
-                'pageVar' => 'page',
+        $criteria         = new CDbCriteria();
+        $criteria->select = 'id, user_id, title, description, create_date';
+        $criteria->order  = 'id desc';
+
+        $dataProvider     = new CActiveDataProvider('Article', array(
+            'criteria'   => $criteria,
+            'pagination' => array(
+                'pageSize' => 20,
+                'route'    => 'index/index',
+                'pageVar'  => 'page',
             ),
         ));
-        $data = compact('dataProvider');
+        $data             = compact('dataProvider');
         $this->render('/home/index', $data);
     }
 
     public function actionError()
     {
-        if($error = Yii::app()->errorHandler->error)
-        {
-            if(Yii::app()->request->isAjaxRequest)
+        if ($error = Yii::app()->errorHandler->error) {
+            if (Yii::app()->request->isAjaxRequest)
                 echo $error['message'];
             else
                 $this->render('//_public/error', $error);
