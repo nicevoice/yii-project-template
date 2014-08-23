d284f2c7d460d3a1c2bb0b3965e78efa08987189
Rogee <rogeecn@gmail.com>
2014-08-23 09:33:31 +0800

    添加标签

--- common/extensions/E_Ad/views/article-content-bottom.php
+++ common/extensions/E_Ad/views/article-content-bottom.php
@@ -1,11 +1,4 @@
-<table>
-    <tr>
-        <td></td>
-        <td></td>
-    </tr>
-    <tr>
-        <td colspan="2">
-
-        </td>
-    </tr>
-</table>
+<!--cnzz tui-->
+<script  type="text/javascript" c=gd  charset="utf-8"  src="http://tui.cnzz.net/cs.php?id=1000082959"></script>
+<!--cnzz tui-->
+		
